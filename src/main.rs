f46ce1b579173ab378782f89b8379e039a2ac40f
oberien <jaro.fietz@gmx.de>
2022-10-23 19:31:56 +0200

    Delta Scatterplot

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use image::io::Reader;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use clap::{builder::ValueParser, Parser, ValueEnum, error::ErrorKind, CommandFactory};
 use either::Either;
+use plotters::backend::BitMapBackend;
+use plotters::chart::ChartBuilder;
+use plotters::drawing::IntoDrawingArea;
+use plotters::element::Circle;
+use plotters::style::{BLUE, Color, GREEN, RED, WHITE};
+use rayon::slice::ParallelSlice;
 
 mod helpers;
 mod registration;
@@ -92,6 +98,61 @@ fn main() {
         .skip(args.skip_files)
         .take(args.num_files)
         .collect();
+
+    let res = files.par_windows(2)
+        .map(|paths| (
+            Reader::open(&paths[0]).unwrap().decode().unwrap().into_rgb64f(),
+            Reader::open(&paths[1]).unwrap().decode().unwrap().into_rgb64f()
+        )).fold(|| Vec::new(), |mut diffs, (left, right)| {
+            let processing_akaze = &[Postprocessing::Maxscale, Postprocessing::Blur(20.), Postprocessing::Sobel(0), Postprocessing::Maxscale, Postprocessing::Akaze(0.001)];
+            let processing_sod = &[Postprocessing::Maxscale, Postprocessing::Blur(20.), Postprocessing::Maxscale, Postprocessing::SingleObjectDetection(0.2)];
+            let processing_aba = &[Postprocessing::Maxscale, Postprocessing::Blur(20.), Postprocessing::Maxscale, Postprocessing::AverageBrightnessAlignment(0.2)];
+            let ref_akaze = registration::prepare_reference_image(left.clone(), files.len(), processing_akaze);
+            let ref_sod = registration::prepare_reference_image(left.clone(), files.len(), processing_sod);
+            let ref_aba = registration::prepare_reference_image(left.clone(), files.len(), processing_aba);
+            let akaze = registration::register(&ref_akaze, right.clone(), files.len(), processing_akaze, false).unwrap_or((0, 0));
+            let sod = registration::register(&ref_sod, right.clone(), files.len(), processing_sod, false).unwrap_or((0, 0));
+            let aba = registration::register(&ref_aba, right.clone(), files.len(), processing_aba, false).unwrap_or((0, 0));
+            diffs.push((akaze, sod, aba));
+            diffs
+        }).reduce(|| Vec::new(), |mut a, b| {
+            a.extend(b);
+            a
+        });
+
+    let (maxabsx, maxabsy) = res.iter().copied()
+        .fold((i32::MIN, i32::MIN), |(maxabsx, maxabsy), diff| {
+            let ((dx1, dy1), (dx2, dy2), (dx3, dy3)) = diff;
+            (
+                maxabsx.max(dx1.abs()).max(dx2.abs()).max(dx3.abs()),
+                maxabsy.max(dy1.abs()).max(dy2.abs()).max(dy3.abs()),
+            )
+        });
+
+    let root = BitMapBackend::new("registration-scatter.png", (1920, 1080)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+
+    let mut scatter_ctx = ChartBuilder::on(&root)
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .build_cartesian_2d(-maxabsx as f32 -1.0..maxabsx as f32+1.0, -maxabsy as f32-1.0..maxabsy as f32+1.0).unwrap();
+    scatter_ctx
+        .configure_mesh()
+        .disable_x_mesh()
+        .disable_y_mesh()
+        .draw().unwrap();
+    scatter_ctx.draw_series(
+        res.iter().copied()
+            .flat_map(|(a, b, c)| [
+                ((a.0 as f32, a.1 as f32), RED),
+                ((b.0 as f32 + 0.3, b.1 as f32), GREEN),
+                ((c.0 as f32, c.1 as f32 + 0.3), BLUE),
+            ]).map(|((x, y), col)| Circle::new((x, y), 2, col.filled())),
+    ).unwrap();
+
+    root.present().unwrap();
+
+    panic!("done");
 
     let first = Reader::open(&files[0]).unwrap().decode().unwrap().into_rgb64f();
     let (width, height) = first.dimensions();
